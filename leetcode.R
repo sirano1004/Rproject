9851b8c18c5d790b707d83b3dcfb8fd7fc8736ba
Hyung Gyu Rho <hyunggyurho@Hyungui-MacBookPro.local>
2022-04-07 22:43:39 +0900

    includes leetcode Q3

--- leetcode.R
+++ leetcode.R
@@ -1,7 +1,29 @@
-print('hello')
+#Leetcode Q3
 
+lengthOfLongestSubstring <- function(str){
+    res <- 0
+    max_res <- res
+    memo <- list()
+    for(i in 1:nchar(str)){
+        char <- substr(str, i, i)
+        if(char %in% names(memo)){
+            res <- 1
+            memo <- list()
+            memo[char] = 1
+        }
+        else{
+            memo[char] = 1
+            res <- res + 1
+            if(res > max_res){
+                max_res <- res
+            }
+        }
+    }
+    max_res
+}
 
-z <- 3.
-print(x  + z)
+examples <- c('abcabcbb', 'bbbbb', 'pwwkew', 'asdkflkasf' ,'ldkfhgsuheg', 'adshgfadkhjfg')
 
-print(x + y + z)
+for(test in examples){
+    print(lengthOfLongestSubstring(test))
+}
