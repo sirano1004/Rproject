b2bedb978228089918057e50df5f029f66972d7c
Hyung Gyu Rho <hyunggyurho@192-168-1-117.tpgi.com.au>
2022-04-11 21:07:41 +1000

    fixed Q22

--- leetcodeQ22.R
+++ leetcodeQ22.R
@@ -1,29 +1,33 @@
 genParen <- function(n){
     addParen <- function(res, left, right, cand, count){
-        if(left > 0 & left <= right){
-        
+        print(left)
+        print(right)
+        print(cand)
+        if(left >= 0 & left <= right){
+            
             if(left == 0 & right == 0){
                 res[[length(res) + 1]] <- cand
+                res
             }
             else{
                 if(left > 0){
                     cand[count] <- "("
-                    addParen(res, left - 1, right, cand, count + 1)
+                    res <- addParen(res, left - 1, right, cand, count + 1)
                 }
 
                 if(right > 0){
                     cand[count] <- ")"
-                    addParen(res, left, right - 1, cand, count + 1)
+                    res <- addParen(res, left, right - 1, cand, count + 1)
                 }
             }
         }
+        print(cand)
+        res
     }
     cand <- character(2 * n)
     res <- list()
-    addParen(res, n, n, cand, 0)
+    res <- addParen(res, n, n, cand, 1)
     res
 }
 
-
-print(genParen(2))
-
+print(genParen(3))
