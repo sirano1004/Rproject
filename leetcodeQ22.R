798cef8149db8fd8470685352cab6f4959cb8eb5
Hyung Gyu Rho <hyunggyurho@192-168-1-117.tpgi.com.au>
2022-04-11 20:32:04 +1000

    add Q22 solution --incomplete

---
+++ leetcodeQ22.R
@@ -0,0 +1,20 @@
+genParen <- function(n){
+    addParen <- function(res, left, right, cand, count){
+        stopofnot(left > 0 & left < right)
+        
+        if(left == 0 & right == 0){
+            append(res, cand)
+        }
+        else{
+            if(left > 0){
+                
+            }
+        }
+    }
+}
+
+a <- "abcd"
+substr(a, 1 , 1) <- 'e'
+print(a)
+
+
